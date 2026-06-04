ee9b118e5bc9ad9ec4e91f9a27f99571c18fe603
Daniel Cosme <cosmedaniel8@gmail.com>
2026-06-04 17:24:00 -0400

    Fix curious-ape tag 1.2.3

--- pkg/services/services.go
+++ pkg/services/services.go
@@ -9,7 +9,7 @@ const (
 
 	CuriousApe      = "curious-ape"
 	CuriusApeHost   = "ape.danicos.me"
-	CuriousApeImage = "danicos.dev/daniel/curious-ape:v1.2.3"
+	CuriousApeImage = "danicos.dev/daniel/curious-ape:1.2.3"
 	CuriuosApePort  = 4000
 
 	Temporal            = "temporal"
